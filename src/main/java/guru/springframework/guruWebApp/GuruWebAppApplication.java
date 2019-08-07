953b78ccc54ea6348826401e616c35a390b6024f
Oksana Bosenko <skybearf@gmail.com>
2019-08-07 12:45:57 +0300

    Initial commit

---
+++ src/main/java/guru/springframework/guruWebApp/GuruWebAppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.guruWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuruWebAppApplication.class, args);
+	}
+
+}
